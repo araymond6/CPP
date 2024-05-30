f9a3c67037a258f643da1135b2a7ed13cb6b2ecf
araymond6 <aurelia.raymond21@gmail.com>
2024-05-30 13:05:16 -0400

    modified CPP08/ex01 main for more random value

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -3,16 +3,17 @@
 int main()
 {
 	std::vector<int> vec;
+	srand(time(NULL));
 	for (int i = 0; i < 10; i++)
 	{
-		vec.push_back(25 + i + i*15); // just to get numbers
+		vec.push_back(rand() % 4000 - 2000); // just to get numbers
 	}
 
 	try
 	{
 		Span span(12);
-		span.addNumber(-9000000);
-		span.addNumber(9000000);
+		span.addNumber(-950);
+		span.addNumber(1585);
 		span.insert(10, vec.begin(), vec.end()); // to fill span with vec, 10 is the amount of elements in vec
 
 		std::cout << "Size: " << span.size() << std::endl;
